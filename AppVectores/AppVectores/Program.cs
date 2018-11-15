0cd43bfa74d586c15c3986b8446698d15c258e05
estudiante <estudiante@L-PCT-117>
2018-11-15 09:49:38 -0500

    Commit inicial

---
+++ AppVectores/AppVectores/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppVectores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Control de versiones");
+            Console.ReadKey();
+        }
+    }
+}
