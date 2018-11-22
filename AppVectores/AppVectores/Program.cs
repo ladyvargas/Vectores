8704a018219756a3a97458dc66cbad959750f4e4
estudiante <estudiante@L-PCT-117>
2018-11-22 09:33:54 -0500

    Funciones

--- AppVectores/AppVectores/Program.cs
+++ AppVectores/AppVectores/Program.cs
@@ -16,7 +16,30 @@ namespace AppVectores
             Console.WriteLine("Control de versiones4");
             Console.WriteLine("Control de versiones5");
             Console.WriteLine("Probando control de versiones");
+            Console.WriteLine("3 Elevado a la 4 es igual a: {0}", potencia(3,4));
+            try
+            {
+                Console.WriteLine("5 Elevado a la 4 es igual a: {0}", potencia(-5, 4));
+            }catch(Exception ex)
+            {
+                Console.WriteLine("Error:" + ex.Message.ToString());
+            }
+            Console.WriteLine("2 Elevado a la 5 es igual a: {0}", potencia(2, 5));
             Console.ReadKey();
         }
-    }
+    public static double potencia(double x, double y)
+        {
+            if (x <= 0)
+                throw new Exception("X debe ser positivo");
+            if (y<=0)
+                throw new Exception("Y debe ser positivo");
+
+        double po=1;
+        for (int i=1; i<=y; i++)
+        {
+            po = po * x;
+        }
+        return po;
+        }
+}
 }
