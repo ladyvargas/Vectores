458fcc47ebb97da79f65c7b80208a9ce4b3b797e
estudiante <estudiante@L-PCT-117>
2018-12-03 08:06:42 -0500

    Vector, sumatoria y promedio

--- AppVectores/AppVectores/Program.cs
+++ AppVectores/AppVectores/Program.cs
@@ -16,30 +16,59 @@ namespace AppVectores
             Console.WriteLine("Control de versiones4");
             Console.WriteLine("Control de versiones5");
             Console.WriteLine("Probando control de versiones");
-            Console.WriteLine("3 Elevado a la 4 es igual a: {0}", potencia(3,4));
+            Console.WriteLine("3 Elevado a la 4 es igual a: {0}", potencia(3, 4));
             try
             {
                 Console.WriteLine("5 Elevado a la 4 es igual a: {0}", potencia(-5, 4));
-            }catch(Exception ex)
+            }
+            catch (Exception ex)
             {
                 Console.WriteLine("Error:" + ex.Message.ToString());
             }
             Console.WriteLine("2 Elevado a la 5 es igual a: {0}", potencia(2, 5));
+            vectorInicializar();
             Console.ReadKey();
+
         }
-    public static double potencia(double x, double y)
+        public static double potencia(double x, double y)
         {
             if (x <= 0)
                 throw new Exception("X debe ser positivo");
-            if (y<=0)
+            if (y <= 0)
                 throw new Exception("Y debe ser positivo");
 
-        double po=1;
-        for (int i=1; i<=y; i++)
+            double po = 1;
+            for (int i = 1; i <= y; i++)
+            {
+                po = po * x;
+            }
+            return po;
+        }
+
+        public static void vectorInicializar()
         {
-            po = po * x;
+            Console.WriteLine("\nELEMENTOS DEL VECTOR\n***************\n");
+            int[] x = new int[8];
+            x[0] = 10;
+            x[1] = 2;
+            x[2] = 20;
+            x[3] = 8;
+            x[4] = 15;
+            x[5] = 1;
+            x[6] = -9;
+            x[7] = 14;
+            for (int i = 0; i< 8; i++)
+            {
+                Console.WriteLine("Elemento indice {0} ---> {1}", (i + 1), x[i]);
+            }
+
+            int suma = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                suma = suma + x[i];
+            }
+            Console.WriteLine("La sumatoria es {0}", suma);
+            Console.WriteLine("El promedio es {0}", (double)suma/x.Length);
         }
-        return po;
-        }
-}
+    }
 }
